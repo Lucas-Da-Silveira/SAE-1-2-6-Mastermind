27b4a869f4e4402edcbb7dbfca2e7bfc2c684abb
xeway <xeway@protonmail.com>
2023-04-14 14:11:11 +0200

    Created class ColorsBoard

---
+++ src/model/ColorsBoard.java
@@ -0,0 +1,14 @@
+package model;
+
+import boardifier.model.GameStageModel;
+import boardifier.model.GridElement;
+
+public class ColorsBoard extends GridElement {
+    public ColorsBoard(int x, int y, int nbRows, int nbCols, GameStageModel gameStageModel) {
+        super("colorsboard", x, y, nbRows, nbCols, gameStageModel);
+    }
+
+    public ColorsBoard(int x, int y, GameStageModel gameStageModel) {
+        super("colorsboard", x, y, 2, 2, gameStageModel);
+    }
+}
