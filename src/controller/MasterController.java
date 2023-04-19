e3e7fc325b440ab043e5bd4fb5f42322e1404706
xeway <xeway@protonmail.com>
2023-04-19 22:33:49 +0200

    Implemented base code in controller

--- src/controller/MasterController.java
+++ src/controller/MasterController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import boardifier.control.ActionPlayer;
 import boardifier.control.Controller;
 import boardifier.model.Model;
+import boardifier.model.Player;
 import boardifier.view.View;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MasterController extends Controller {
@@ -13,6 +16,7 @@ public class MasterController extends Controller {
 
     public MasterController(Model model, View view) {
         super(model, view);
+        firstPlayer = true;
     }
 
     @Override
@@ -25,5 +29,39 @@ public class MasterController extends Controller {
         }
         stopStage();
         endGame();
+    }
+
+    public void nextPlayer() {
+        if (!firstPlayer) {
+            model.setNextPlayer();
+        } else {
+            firstPlayer = false;
+        }
+
+        Player p = model.getCurrentPlayer();
+        if (p.getType() == Player.COMPUTER) {
+            System.out.println("COMPUTER PLAYS");
+            MasterDecider decider = new MasterDecider(model, this);
+            ActionPlayer play = new ActionPlayer(model, this, decider, null);
+            play.start();
+        } else {
+            boolean ok = false;
+            while (!ok) {
+                System.out.print(p.getName()+ " > ");
+                try {
+                    String line = consoleIn.readLine();
+                    if (line.length() == 4) {
+                        ok = analyseAndPlay(line);
+                    }
+                    if (!ok) {
+                        System.out.println("incorrect instruction. retry !");
+                    }
+                } catch(IOException e) {}
+            }
+        }
+    }
+
+    public boolean analyseAndPlay(String line) {
+        // jouer l'input de l'utilisateur
     }
 }
--- src/controller/MasterDecider.java
+++ src/controller/MasterDecider.java
@@ -1,5 +1,15 @@
 package controller;
 
-public class MasterDecider {
+import boardifier.control.Controller;
+import boardifier.control.Decider;
+import boardifier.model.Model;
+import boardifier.model.action.ActionList;
 
+public class MasterDecider extends Decider {
+    public MasterDecider(Model model, Controller control) {
+        super(model, control);
+    }
+
+    @Override
+    public ActionList decide() {}
 }
