880512ade1f32badfb8304f0f48eba65cb8b6645
xeway <xeway@protonmail.com>
2023-04-27 08:58:18 +0200

    Added test folder

---
+++ test/controller/MasterControllerUnitTest.java
@@ -0,0 +1,16 @@
+package controller;
+
+import boardifier.model.GameStageModel;
+import boardifier.model.Model;
+import model.MasterStageModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MasterControllerUnitTest {
+    @Test
+    public void testVerifyLine() {
+
+    }
+}
